55b3b79673fd1c8ecd54c857ee7f3797291af141
DeAlchemist641 <139173971+DeAlchemist641@users.noreply.github.com>
2025-07-25 10:04:40 +0300

    Update settings.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,5 @@
 module.exports = {
   SESSION_ID: "malvin~0r1D3SKD#R7wmI2xs5kOh2co7f0XSJ3PqWWt20bjVrGQHGTMEZIg",  // add your Session Id make sure it starts with malvin~
   OWNER_NUMBER: "254787002739", // PUT OWNER NUMBER HERE
-  PREFIX: "#", // prefix (e.g., ., /, !, *)
-  MODE: "private"
+  PREFIX: "#"// prefix (e.g., ., /, !, *)
 };
